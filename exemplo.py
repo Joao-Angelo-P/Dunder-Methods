6225403e3f3a591f935c038da3d064df84297af8
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-08-12 11:08:25 -0400

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -27,29 +27,45 @@ class A:
         else:
             raise StopIteration
 
-# Testando os metodos
+class B:
+    """
+    Objeto que pode gerar um iteravel so com __getitem__ e bool com __len__
+    """
+    def __init__(self, arr: List):
+        self.arr = arr
 
-# 1) __init__
-a = A(list(range(1, 11, 1)))
+    def __getitem__(self, index: Union[int, slice]):
+        return self.arr[index]
 
-# 2) __len__
-print(f'Tamanho do objeto:\t{len(a)}')
+    def __len__(self):
+        return len(self.arr)
 
-# 3) __getitem__
-print(f'Com tipo \'int\':\n3° elemento: {a[2]} ''\nCom tipo \'slice\':\nde 1° a 4° elemento:\t{0} '.format(a[:4]))
+class C:...
 
-# 4) __bool__
-valor = True if a else False
-print(f"O valor logico: {valor}")
 
-# 5)__iter__
-iter_ = iter(a)
-print(f"Estrutura iteravel {iter_}")
-
-# 5) __next__
-try:
-  while (a:=next(iter_)):
-    print(f"Item da iteracao: {a}")
-  
-except StopIteration:
- print("\n--Acabou a iteracao--")
+if __name__ == '__main__':    # Testando os metodos
+    
+    # 1) __init__
+    a = A(list(range(1, 11, 1)))
+    
+    # 2) __len__
+    print(f'Tamanho do objeto:\t{len(a)}')
+    
+    # 3) __getitem__
+    print(f'Com tipo \'int\':\n3° elemento: {a[2]} ''\nCom tipo \'slice\':\nde 1° a 4° elemento:\t{0} '.format(a[:4]))
+    
+    # 4) __bool__
+    valor = True if a else False
+    print(f"O valor logico: {valor}")
+    
+    # 5)__iter__
+    iter_ = iter(a)
+    print(f"Estrutura iteravel {iter_}")
+    
+    # 5) __next__
+    try:
+      while (a:=next(iter_)):
+        print(f"Item da iteracao: {a}")
+      
+    except StopIteration:
+     print("\n--Acabou a iteracao--")
